34b656625381cc87be5fc9bc48fb8a7ef1901000
RenjarP7 <15037698+RenjarP7@users.noreply.github.com>
2020-07-05 21:15:17 +0200

    Add files via upload

---
+++ sendMSG.py
@@ -0,0 +1,44 @@
+#    Friendly Telegram (telegram userbot)
+#    Copyright (C) 2018-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from .. import loader, utils
+import logging
+import asyncio
+
+
+logger = logging.getLogger(__name__)
+
+
+@loader.tds
+class SendMsgMod(loader.Module):
+    """Executes a send message to a Telegram ID"""
+    strings = {"name": "SendMsg",
+               "need_id_msg": "<b>Need Chat ID and Message.</b>",
+               "need_msg": "<b>Need Message.</b>"}
+
+    async def sendcmd(self, message):
+        """.send <id> <message>"""
+        use_reply = False
+        args = utils.get_args(message)
+        logger.debug(args)
+        logger.debug(args[0])
+        if len(args) == 0:
+            await utils.answer(message, self.strings("need_id_msg", message))
+            return
+        if len(args) == 1:
+            await utils.answer(message, self.strings("need_msg", message))
+            return
+        await message.client.send_message(args[0], " ".join(args[1:]))
