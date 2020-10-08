8940114b908c3dc686c3ed57592dd2a2fd033cac
RenjarP7 <15037698+RenjarP7@users.noreply.github.com>
2020-10-08 22:45:00 +0200

    Update sendMSG.py
    Format update

--- sendMSG.py
+++ sendMSG.py
@@ -2,8 +2,8 @@
 #    Copyright (C) 2018-2019 The Authors
 
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 
 #    This program is distributed in the hope that it will be useful,
@@ -16,7 +16,6 @@
 
 from .. import loader, utils
 import logging
-import asyncio
 
 
 logger = logging.getLogger(__name__)
@@ -26,30 +25,39 @@ logger = logging.getLogger(__name__)
 class SendMsgMod(loader.Module):
     """Sends a message to a Telegram account."""
     strings = {"name": "SendMsg",
-               "need_id_msg": "<b>Need Chat ID / Phone number and Message.</b>",
-               "need_msg": "<b>Need Message.</b>", 
-               "config_true": "<b>Config successfully updated!\nMessage will be deleted.</b>", 
-               "config_false": "<b>Config successfully updated!\nMessage will be kept.</b>",
-               "config_fail": "<b>Failed to update config!\nOnly \'true\' or \'false\' permitted!\n.sendconfig &lt;true_or_false&gt;</b>", 
-               "config_need": "<b>Need true or false.\n.sendconfig &lt;true_or_false&gt;</b>"}
+               "need_id_msg": "<b>Need Chat ID / Phone number and Message."
+               "</b>",
+               "need_msg": "<b>Need Message.</b>",
+               "config_true": "<b>Config successfully updated!\nMessage will "
+               "be deleted.</b>",
+               "config_false": "<b>Config successfully updated!\nMessage will "
+               "be kept.</b>",
+               "config_fail": "<b>Failed to update config!\nOnly \'true\' or "
+               "\'false\' permitted!\n.sendconfig &lt;true_or_false&gt;</b>",
+               "config_need": "<b>Need true or false.\n.sendconfig "
+               "&lt;true_or_false&gt;</b>"}
 
     async def client_ready(self, client, db):
         self._db = db
 
     async def sendconfigcmd(self, message):
-        """.sendconfig <true_or_false>\ne.g. .sendconfig false\nIf set to true, command message will be deleted after message was send."""
+        """.sendconfig <true_or_false>\ne.g. .sendconfig false
+If set to true, command message will be deleted after message was send."""
         args = utils.get_args(message)
         if len(args) == 0:
-            await utils.answer(message, self.strings("config_need", message)) 
+            await utils.answer(message, self.strings("config_need", message))
         else:
-            if args[0] == "true": 
+            if args[0] == "true":
                 self._db.set(__name__, "deleteBotMsg", True)
-                await utils.answer(message, self.strings("config_true", message))
-            elif args[0] == "false": 
+                await utils.answer(message,
+                                   self.strings("config_true", message))
+            elif args[0] == "false":
                 self._db.set(__name__, "deleteBotMsg", False)
-                await utils.answer(message, self.strings("config_false", message))
-            else: 
-                await utils.answer(message, self.strings("config_fail", message))
+                await utils.answer(message,
+                                   self.strings("config_false", message))
+            else:
+                await utils.answer(message,
+                                   self.strings("config_fail", message))
 
     async def sendcmd(self, message):
         """.send <id> <message>\ne.g. .send @user Hello World!"""
@@ -61,17 +69,26 @@ class SendMsgMod(loader.Module):
             await utils.answer(message, self.strings("need_msg", message))
             return
         try:
-            await message.client.send_message(args[0], message.message.split(" ".join(args[0:1]))[1])
+            await message.client.send_message(args[0],
+                                              message.message.split(" "
+                                              .join(args[0:1]))[1])
             try:
                 if self._db.get(__name__, "deleteBotMsg", False):
-                    await message.client.delete_messages(message.to_id, message.id)
+                    await message.client.delete_messages(message.to_id,
+                                                         message.id)
             except ValueError as e:
                 logger.debug("Failed to delete message.")
-                await utils.answer(message, "Failed to delete message.\nSee logs for more information.\n.logs debug FORCE_INSECURE\nDon't share it, it contains private information.") 
+                await utils.answer(message, "Failed to delete message.\nSee"
+                                   " logs for more information.\n.logs "
+                                   "debug FORCE_INSECURE\nDon't share it, "
+                                   "it contains private information.")
                 logger.debug(e)
                 return
         except ValueError as e:
             logger.debug("Failed to send message.")
-            await utils.answer(message, "Failed to send message.\nSee logs for more information.\n.logs debug FORCE_INSECURE\nDon't share it, it contains private information.") 
+            await utils.answer(message, "Failed to send message.\nSee "
+                               "logs for more information.\n.logs debug "
+                               "FORCE_INSECURE\nDon't share it, it contains "
+                               "private information.")
             logger.debug(e)
             return
